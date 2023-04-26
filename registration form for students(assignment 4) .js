8407c23d7d2972f6012afb3107facb2ee506d256
yashb079 <93524837+yashb079@users.noreply.github.com>
2023-04-25 23:01:30 -0700

    Create registration form for students(assignment 4) .js

---
+++ registration form for students(assignment 4) .js
@@ -0,0 +1,8 @@
+function validateForm() {
+	// Get form values
+	var fullName = document.forms["registrationForm"]["fullName"].value;
+	var email = document.forms["registrationForm"]["email"].value;
+	var phoneNumber = document.forms["registrationForm"]["phoneNumber"].value;
+	var gender = document.forms["registrationForm"]["gender"].value;
+	var dob = document.forms["registrationForm"]["dob"].value;
+	var address = document.forms["registrationForm"]["address"].
